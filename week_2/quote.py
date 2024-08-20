dba125debcd2f128e40098c9b451964c28eb94f5
Angelica Gutierrez <angelicagu368@gmail.com>
2024-08-19 20:25:58 -0700

    week 2 files and exercises

---
+++ week_2/quote.py
@@ -0,0 +1,15 @@
+""""
+Author: Angelica Gutierrez
+Date: August 19, 2024
+File Name: quote.py
+Description: This file prints a quote. 
+"""
+# Quote varible 
+quote  = "Art is the weapon. Your imagination is the ammunition. Stay Dirty, and Stay Dangerous. Create and Destroy as you see fit. Embrace your Originality. The Aftermath is Secondary. You can and should do Anything."
+
+# Author variable
+author = " - Frank Iero"
+
+# Printing quote & author
+print(quote)
+print(author)
